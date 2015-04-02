e42cf1a179db425c37180011f0e7c45754f1e896
crazyyy <crazyyy@gmail.com>
2015-04-02 10:14:20 +0300

    product page preready

--- wordpress/wp-content/themes/wp-kovkanazakaz/product-page.php
+++ wordpress/wp-content/themes/wp-kovkanazakaz/product-page.php
@@ -7,72 +7,86 @@
         <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
     </div><!-- breadcrumbs -->
 
-
     <ul class="list-inline separateWorksList" itemtype="http://schema.org/ItemList" itemscope="itemscope">
-
+        <?php
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            $temp = $wp_query;
+            $wp_query= null;
+            query_posts('post_type=products'.'&showposts=100'.'&paged='.$paged);
+            while (have_posts()) : the_post();
+        ?>
 
         <li itemtype="http://schema.org/Product" itemprop="itemListElement" itemscope="itemscope" class="class-b">
-            <meta itemprop="image" content="http://kovka-na-zakaz.ru/upload/kovka-na-zakaz/information_system_19/1/2/8/item_1289/small_information_items_1289.jpg">
-            <meta itemprop="name" content="Перила №1289">
-            <div class="work-image-container"><img class="work-image thezoom lazy the-zoom-img the-zoom-zoom_in" data-id="1289" thezoom-bigsrc="<?php echo get_template_directory_uri(); ?>//upload/kovka-na-zakaz/information_system_19/1/2/8/item_1289/information_items_1289.jpg" src="<?php echo get_template_directory_uri(); ?>/img/small_information_items_1289.jpg" data-no-retina="true" width="192" height="144" alt="" title="">
+            <meta itemprop="image" 
+                content="http://kovka-na-zakaz.ru/upload/kovka-na-zakaz/information_system_19/1/2/8/item_1289/small_information_items_1289.jpg">
+                <!-- big image link -->
+            <meta 
+                itemprop="name" 
+                content="<?php the_title(); ?>">
+            <div class="work-image-container">
+                <img 
+                    class="work-image thezoom lazy the-zoom-img the-zoom-zoom_in" 
+                    data-id="<?php the_ID(); ?>" 
+                    thezoom-bigsrc="<?php echo get_template_directory_uri(); ?>//upload/kovka-na-zakaz/information_system_19/1/2/8/item_1289/information_items_1289.jpg" src="<?php echo get_template_directory_uri(); ?>/img/small_information_items_1289.jpg" 
+                    data-no-retina="true" 
+                    width="192" 
+                    height="144" 
+                    alt="<?php the_title(); ?>" 
+                    title="<?php the_title(); ?>">
+                    <!-- big image link -->
             </div>
-            <p class="text-center work-favourite-link"><a href="http://kovka-na-zakaz.ru/works/railings/#favourite_1289" title="Отметить работу" data-id="1289" class="">Артикул 1289</a><small>Добавить в избранное</small>
-            </p>
-        </li>
-        <li itemtype="http://schema.org/Product" itemprop="itemListElement" itemscope="itemscope" class="class-b">
-            <meta itemprop="image" content="http://kovka-na-zakaz.ru/upload/kovka-na-zakaz/information_system_19/1/1/4/item_1146/small_information_items_1146.jpg">
-            <meta itemprop="name" content="Перила №1146">
-            <div class="work-image-container"><img class="work-image thezoom lazy the-zoom-img the-zoom-zoom_in" data-id="1146" thezoom-bigsrc="<?php echo get_template_directory_uri(); ?>//upload/kovka-na-zakaz/information_system_19/1/1/4/item_1146/information_items_1146.jpg" src="<?php echo get_template_directory_uri(); ?>/img/small_information_items_1146.jpg" data-no-retina="true" width="192" height="175" alt="" title="">
-            </div>
-            <p class="text-center work-favourite-link"><a href="http://kovka-na-zakaz.ru/works/railings/#favourite_1146" title="Отметить работу" data-id="1146" class="">Артикул 1146</a><small>Добавить в избранное</small>
-            </p>
-        </li>
-        <li itemtype="http://schema.org/Product" itemprop="itemListElement" itemscope="itemscope" class="class-b">
-            <meta itemprop="image" content="http://kovka-na-zakaz.ru/upload/kovka-na-zakaz/information_system_19/1/0/6/item_1064/small_information_items_1064.jpg">
-            <meta itemprop="name" content="Перила №1064">
-            <div class="work-image-container"><img class="work-image thezoom lazy the-zoom-img the-zoom-zoom_in" data-id="1064" thezoom-bigsrc="<?php echo get_template_directory_uri(); ?>//upload/kovka-na-zakaz/information_system_19/1/0/6/item_1064/information_items_1064.jpg" src="<?php echo get_template_directory_uri(); ?>/img/small_information_items_1064.jpg" data-no-retina="true" width="192" height="185" alt="" title="">
-            </div>
-            <p class="text-center work-favourite-link"><a href="http://kovka-na-zakaz.ru/works/railings/#favourite_1064" title="Отметить работу" data-id="1064" class="">Артикул 1064</a><small>Добавить в избранное</small>
+            <p class="text-center work-favourite-link">
+                <a 
+                    href="http://kovka-na-zakaz.ru/works/railings/#favourite_<?php the_ID(); ?>" 
+                    title="Отметить работу" 
+                    data-id="<?php the_ID(); ?>">Артикул <?php the_ID(); ?>
+                </a>
+                <small>Добавить в избранное</small>
             </p>
         </li>
 
+        <?php endwhile; ?>
+        <?php wp_reset_query(); ?>
+    </ul><!-- list-inline separateWorksList -->
 
+    <div class="main-contenta">
         <?php the_content(); ?>
+    </div>
+    <!-- /.main-contenta -->
 
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/gsap.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/thezoom.min.js"></script>
+    <script type="text/javascript">
+        $(function () {
 
-        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/gsap.min.js"></script>
-        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/thezoom.min.js"></script>
-        <script type="text/javascript">
-            $(function () {
+            $("img.thezoom").thezoom({
+                rel: 'works',
+                easing: 'easeQuad',
+                hover_enabled: false,
+                hover_scale: 1.4,
+                hover_duration: 100,
+                overlay_opacity: 0.5,
+                close_button_text: 'Закрыть',
+                social_buttons_enabled: false
+            });
 
-                $("img.thezoom").thezoom({
-                    rel: 'works',
-                    easing: 'easeQuad',
-                    hover_enabled: false,
-                    hover_scale: 1.4,
-                    hover_duration: 100,
-                    overlay_opacity: 0.5,
-                    close_button_text: 'Закрыть',
-                    social_buttons_enabled: false
-                });
-
-                $('img.thezoom').bind('click', function () {
-                    var item_id = $(this).data('id');
-                    $.ajax({
-                        url: "./",
-                        type: "POST",
-                        data: {
-                            id: item_id,
-                            makeviewpost: true
-                        },
-                        dataType: "json"
-                    });
+            $('img.thezoom').bind('click', function () {
+                var item_id = $(this).data('id');
+                $.ajax({
+                    url: "./",
+                    type: "POST",
+                    data: {
+                        id: item_id,
+                        makeviewpost: true
+                    },
+                    dataType: "json"
                 });
             });
-        </script>
+        });
+    </script>
+    <!--для эффектов перехода-->
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-ui.min.js"></script>
 
-        <!--для эффектов перехода-->
-        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-ui.min.js"></script>
 </div>
 </div>
 </div>
