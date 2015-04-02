2cdb9543fad9c632a22dce944350d55e3dc508d0
crazyyy <crazyyy@gmail.com>
2015-04-02 17:28:10 +0300

    ready works page template

--- wordpress/wp-content/themes/wp-kovkanazakaz/page-works.php
+++ wordpress/wp-content/themes/wp-kovkanazakaz/page-works.php
@@ -1,211 +1,61 @@
 <?php /* Template Name: Works Page Template */ get_header(); ?>
-    
-    <div class="page-real-content clearfix">
-        <h1><?php the_title(); ?></h1>
-        <div class="breadcrumbs">
-            <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
-        </div><!-- breadcrumbs -->
 
+<div class="page-real-content ready-works clearfix">
+    <h1><?php the_title(); ?></h1>
+
+    <div class="breadcrumbs">
+        <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+    </div><!-- breadcrumbs -->
+
+    <ul class="list-inline separateWorksList" itemtype="http://schema.org/ItemList" itemscope="itemscope">
+        <?php
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            $temp = $wp_query;
+            $wp_query= null;
+            query_posts('post_type=products'.'&showposts=100'.'&paged='.$paged);
+            while (have_posts()) : the_post();
+        ?>
+        <li itemtype="http://schema.org/ImageObject" itemscope="itemscope">
+            <?php 
+                $images = get_field('gallery'); 
+                $image_1 = $images[0]; 
+            ?>
+            <meta 
+                itemprop="contentUrl" 
+                content="<?php echo $image_1[url]; ?>"
+            >
+            <meta itemprop="name" content="<?php the_title(); ?> №<?php the_ID(); ?>">
+            <div >
+            <a href="<?php echo $image_1[url]; ?>" class="work-image-container" rel="lightbox">
+                <img 
+                    class="work-image thezoom lazy the-zoom-img the-zoom-zoom_in" 
+                    data-id="<?php the_ID(); ?>" 
+                    src="<?php echo $image_1[url]; ?>" 
+                    width="192" 
+                    height="154" 
+                    alt="<?php the_title(); ?>" 
+                    title="<?php the_title(); ?>"
+                >
+            </a>
+            </div>
+            <p class="text-center work-favourite-link">
+                <a 
+                    href="#favourite_<?php the_ID(); ?>" 
+                    title="Отметить работу" 
+                    data-id="<?php the_ID(); ?>" 
+                >
+                    Артикул <?php the_ID(); ?>
+                </a>
+            </p>
+        </li>
+        <?php endwhile; ?>
+        <?php wp_reset_query(); ?>
+    </ul><!-- list-inline separateWorksList -->
+
+    <div class="main-contenta">
         <?php the_content(); ?>
+    </div>
+    <!-- /.main-contenta -->
 
-              
-                        <div class="listBlocks">
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/railings/" title="Перила">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_25.png" width="200" height="200" alt="Перила" title="Перила">
-                                        </p>
-                                        <p class="text-center">Перила</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/staircase/" title="Лестницы">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_38.png" width="200" height="200" alt="Лестницы" title="Лестницы">
-                                        </p>
-                                        <p class="text-center">Лестницы</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/balcony/" title="Балконы">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_47.png" width="200" height="200" alt="Балконы" title="Балконы">
-                                        </p>
-                                        <p class="text-center">Балконы</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/window-grilles/" title="Оконные решетки">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_29.png" width="200" height="200" alt="Оконные решетки" title="Оконные решетки">
-                                        </p>
-                                        <p class="text-center">Оконные решетки</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/peaks/" title="Козырьки">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_27.png" width="200" height="200" alt="Козырьки" title="Козырьки">
-                                        </p>
-                                        <p class="text-center">Козырьки</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/canopies/" title="Навесы">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_26.png" width="200" height="200" alt="Навесы" title="Навесы">
-                                        </p>
-                                        <p class="text-center">Навесы</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/gazebos/" title="Беседки">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_35.png" width="200" height="200" alt="Беседки" title="Беседки">
-                                        </p>
-                                        <p class="text-center">Беседки</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/fence/" title="Заборы">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_28.png" width="200" height="200" alt="Заборы" title="Заборы">
-                                        </p>
-                                        <p class="text-center">Заборы</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/gates/" title="Ворота">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_37.png" width="200" height="200" alt="Ворота" title="Ворота">
-                                        </p>
-                                        <p class="text-center">Ворота</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/wickets/" title="Калитки">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_34.png" width="200" height="200" alt="Калитки" title="Калитки">
-                                        </p>
-                                        <p class="text-center">Калитки</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/barbecues/" title="Мангалы">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_36.png" width="200" height="200" alt="Мангалы" title="Мангалы">
-                                        </p>
-                                        <p class="text-center">Мангалы</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/firewood-rack/" title="Дровницы">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_46.png" width="200" height="200" alt="Дровницы" title="Дровницы">
-                                        </p>
-                                        <p class="text-center">Дровницы</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/bench/" title="Скамейки">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_33.png" width="200" height="200" alt="Скамейки" title="Скамейки">
-                                        </p>
-                                        <p class="text-center">Скамейки</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/tables/" title="Столы">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_32.png" width="200" height="200" alt="Столы" title="Столы">
-                                        </p>
-                                        <p class="text-center">Столы</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/chairs/" title="Стулья">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_48.png" width="200" height="200" alt="Стулья" title="Стулья">
-                                        </p>
-                                        <p class="text-center">Стулья</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/clothes-racks/" title="Вешалки">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_45.png" width="200" height="200" alt="Вешалки" title="Вешалки">
-                                        </p>
-                                        <p class="text-center">Вешалки</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div>
-                                <a href="http://kovka-na-zakaz.ru/works/lights/" title="Светильники">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_30.png" width="200" height="200" alt="Светильники" title="Светильники">
-                                        </p>
-                                        <p class="text-center">Светильники</p>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="placeholder">
-                                <a href="http://kovka-na-zakaz.ru/works/lamps/" title="Фонари">
-                                    <div>
-                                        <p class="text-center"><img src="<?php echo get_template_directory_uri(); ?>/img/structure_property_image_31.png" width="200" height="200" alt="Фонари" title="Фонари">
-                                        </p>
-                                        <p class="text-center">Фонари</p>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-
-                        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/gsap.min.js"></script>
-                        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/thezoom.min.js"></script>
-                        <script type="text/javascript">
-                            $(function () {
-
-                                $("img.thezoom").thezoom({
-                                    rel: 'works',
-                                    easing: 'easeQuad',
-                                    hover_enabled: false,
-                                    hover_scale: 1.4,
-                                    hover_duration: 100,
-                                    overlay_opacity: 0.5,
-                                    close_button_text: 'Закрыть',
-                                    social_buttons_enabled: false
-                                });
-
-                                $('img.thezoom').bind('click', function () {
-                                    var item_id = $(this).data('id');
-                                    $.ajax({
-                                        url: "./",
-                                        type: "POST",
-                                        data: {
-                                            id: item_id,
-                                            makeviewpost: true
-                                        },
-                                        dataType: "json"
-                                    });
-                                });
-                            });
-                        </script>
-
-                        <!--для эффектов перехода-->
-                        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-ui.min.js"></script>
-                    </div>
+</div><!-- ready-works -->
 <?php get_footer(); ?>
