3e8ca254ff2b27fd21edd34f352bb5123a0c89d3
DAN-STIX <dan.stix87@gmail.com>
2015-04-02 19:00:31 +0300

    blog

--- wordpress/wp-content/themes/wp-kovkanazakaz/index.php
+++ wordpress/wp-content/themes/wp-kovkanazakaz/index.php
@@ -4,7 +4,7 @@
 	<section role="main">
 	
 		<h1 class="ctitle"><?php _e( 'Latest Posts', 'wpeasy' ); ?></h1>
-	
+		<?php get_template_part('pagination'); ?>
 		<?php get_template_part('loop'); ?>
 		
 		<?php get_template_part('pagination'); ?>
