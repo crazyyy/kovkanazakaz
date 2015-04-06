47d15efb0676ccbcad1ab78400a6a1a9231d3d28
DAN-STIX <dan.stix87@gmail.com>
2015-04-06 17:16:44 +0300

    start - ready works on home page

--- wordpress/wp-content/themes/wp-kovkanazakaz/page-home.php
+++ wordpress/wp-content/themes/wp-kovkanazakaz/page-home.php
@@ -27,56 +27,46 @@
             <div class="steps-circle-shadow third"></div>
         </div>
         <?php the_field('content1'); ?>
-<!-- list-inline separateWorksList -->
-           <ul class="list-inline separateWorksList" itemtype="http://schema.org/ItemList" itemscope="itemscope">
-        <?php
-            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-            $temp = $wp_query;
-            $wp_query= null;
-            query_posts('post_type=products_ready'.'&showposts=12'.'&paged='.$paged);
-            while (have_posts()) : the_post();
-        ?>
-        <li itemtype="http://schema.org/ImageObject" itemscope="itemscope">
-        <?php
-            $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' );
-            $url = $thumb['0'];
-        ?>
-            <div>
-            <a href="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );; ?>" class="work-image-container work-image thezoom lazy the-zoom-img the-zoom-zoom_in" rel="lightbox">
 
-            <img 
-                src="<?php echo $url  ?>" 
-                data-id="<?php the_ID(); ?>"
-                title="<?php the_title(); ?>" 
-                alt="<?php the_title(); ?>" 
-            />
+        <!-- 111111111111111 -->
+       
 
-            </a>
-            </div>
-            <p class="text-center work-favourite-link">
-                <a 
-                    href="#favourite_<?php the_ID(); ?>" 
-                    title="Отметить работу" 
-                    data-id="<?php the_ID(); ?>" 
-                >
-                    Артикул <?php the_ID(); ?>
-                </a>
-            </p>
-        </li>
-        <?php endwhile; ?>
-        <?php wp_reset_query(); ?>
-    </ul><!-- list-inline separateWorksList -->
+
+
+<ul class="list-inline separateWorksList mainPageList">
+
+
+
+
+<li><div class="work-image-container">
+<a href="/works/railings/#item-1310" 
+title="Кованые перила">
+<img class="work-image"
+ data-id="1310" 
+ thezoom-bigsrc="/upload/kovka-na-zakaz/information_system_19/1/3/1/item_1310/information_items_1310.jpg" 
+ src="/upload/kovka-na-zakaz/information_system_19/1/3/1/item_1310/small_information_items_1310.jpg" 
+ data-no-retina="true" width="192" height="128" 
+ alt="Кованые перила" title="Кованые перила">
+ </a></div>
+ <p class="text-center">
+ <a href="/works/railings/#item-1310" 
+ title="Кованые перила">Кованые перила</a></p></li>
+
+
+
+
+</ul>
 
 
         <?php the_field('content2'); ?>
 
 
    
-    <div class="main-page-reviews-list clearfix">
+<div class="main-page-reviews-list clearfix">
     
-        <?php if( have_rows('review-body', 357) ): ?>
+    <?php if( have_rows('review-body', 357) ): ?>
 
-            <?php while( have_rows('review-body', 357) ): the_row(); 
+        <?php while( have_rows('review-body', 357) ): the_row(); 
 
                 // vars
                 $name = get_sub_field('name');
@@ -92,26 +82,25 @@
                 $time = strtotime("{$d}-{$m}-{$y}");
                 
             ?>
-<div class="position-1">
-    <div class="review">
-        <div class="review-text-container">
-            <div class="review-text"><time class="review-datetime" datetime="2014-10-10"><?php echo date('d/m/Y', $time); ?></time><?php echo $content; ?>
+            <div class="position-1">
+                <div class="review">
+                    <div class="review-text-container">
+                        <div class="review-text"><time class="review-datetime" datetime="2014-10-10"><?php echo date('d/m/Y', $time); ?></time><?php echo $content; ?>
+                        </div>
+                    </div>
+                    <div class="review-author-container clearfix">
+                        <div class="review-author-image"><img src="<?php echo $foto; ?>" width="50" height="50" alt="" title="">
+                        </div>
+                        <div class="review-author-creds floated">
+                            <div class="review-author"><?php echo $name; ?></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="review-author-container clearfix">
-            <div class="review-author-image"><img src="<?php echo $foto; ?>" width="50" height="50" alt="" title="">
-            </div>
-            <div class="review-author-creds floated">
-                <div class="review-author"><?php echo $name; ?></div>
-            </div>
-        </div>
-    </div>
-</div>
 
 
             <?php endwhile; ?>
         <?php endif; ?>
-     
     </div><!-- page-reviews-list clearfix -->
       
 
