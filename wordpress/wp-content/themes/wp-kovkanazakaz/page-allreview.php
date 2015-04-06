308114a6491ed10590f7fa235b27e8c9fff39fa7
DAN-STIX <dan.stix87@gmail.com>
2015-04-06 11:56:31 +0300

    page-allreview.php

---
+++ wordpress/wp-content/themes/wp-kovkanazakaz/page-allreview.php
@@ -0,0 +1,60 @@
+<?php /* Template Name: Allreview */ get_header(); ?>
+
+
+<div class="page-real-content clearfix">
+    <h1><?php the_title(); ?></h1>
+
+    <div class="breadcrumbs">
+        <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+    </div><!-- breadcrumbs -->
+
+    <ul class="list-inline separateWorksList" itemtype="http://schema.org/ItemList" itemscope="itemscope">
+        <?php
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            $temp = $wp_query;
+            $wp_query= null;
+            query_posts('post_type=products'.'&showposts=100'.'&paged='.$paged);
+            while (have_posts()) : the_post();
+        ?>
+        <li itemtype="http://schema.org/ImageObject" itemscope="itemscope">
+            <?php 
+                $images = get_field('gallery'); 
+                $image_1 = $images[0]; 
+            ?>
+            <meta 
+                itemprop="contentUrl" 
+                content="<?php echo $image_1[url]; ?>"
+            >
+            <meta itemprop="name" content="<?php the_title(); ?> №<?php the_ID(); ?>">
+            <a href="<?php the_permalink(); ?>" class="work-image-container">
+                <img 
+                    src="<?php echo $image_1[url]; ?>" 
+                    width="192" 
+                    height="154" 
+                    alt="<?php the_title(); ?>" 
+                    title="<?php the_title(); ?>"
+                >
+            </a>
+            <p class="text-center work-favourite-link">
+                <?php the_title(); ?><br>
+                Стоимость <?php the_field('price'); ?><img src="<?php echo get_template_directory_uri(); ?>/img/rouble.png"> 
+            </p>
+        </li>
+        <?php endwhile; ?>
+        <?php wp_reset_query(); ?>
+    </ul><!-- list-inline separateWorksList -->
+
+    <div class="main-contenta">
+        <?php the_content(); ?>
+    </div>
+    <!-- /.main-contenta -->
+        <div class="clearfix full-feeedback-form">
+            <div class="feedback-form-left">
+                <?php echo do_shortcode('[contact-form-7 id="141" title="Обратная связь"]'); ?>
+            </div>
+            <div class="feedback-form-right">
+                <?php the_content(); ?>
+            </div>
+        </div>
+</div>
+<?php get_footer(); ?>
